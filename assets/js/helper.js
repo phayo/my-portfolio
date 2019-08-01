bc37d3caf2a462a2bea51bde70a6c1f6469f6a31
mrphayo <anazodogodwin@gmail.com>
2019-08-01 15:15:49 +0100

    add sidebar  and redesign navbar

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,57 +1,14 @@
-//performPageActions();
-$(".button-collapse").sideNav();
-// SideNav Scrollbar Initialization
+
 var sideNavScrollbar = document.querySelector('.custom-scrollbar');
 var ps = new PerfectScrollbar(sideNavScrollbar);
 $(document).ready(function(){
-    
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+   });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+
+        $('.collapse.in').toggleClass('in');
+    });
 });
-
-// function getCurrentPage() {
-//     var path = window.location.pathname;
-//     return path.split("/")[7];
-// }
-
-// function queryServer(method, data, url, callfunction){
-//     $.ajax({
-//         url: url,
-//         type: method,
-//         data: data,
-//         success: function(response){
-//             linkFunction(callfunction, response);
-//         },
-//         error: function (xhr, status) {
-//             switch (status) {
-//                 case 404:
-//                     alert('File not found');
-//                     break;
-//                 case 500:
-//                     alert('Server error');
-//                     break;
-//                 case 0:
-//                     alert('Request aborted');
-//                     break;
-//                 default:
-//                     alert('Unknown error: ' + xhr.statusText);
-//             }
-//         }
-//     });
-// }
-
-// function PriceFormat(amount) {
-//     var formatter = new Intl.NumberFormat('en-US', {
-//         style: 'currency',
-//         currency: 'NGN',
-//         minimumFractionDigits: 2
-//     });
-//     amount = formatter.format(amount);
-//     amount = amount.replace("NGN", "₦");
-//     return amount.replace(".00", "");
-// }
-
-// function getMonth(){
-//     var date = new Date();
-//     var month = date.getMonth();
-//     return month
-// }
-// // SideNav Button Initialization
